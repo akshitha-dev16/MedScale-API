1812ed0d63c351106751a446e1e0b5f62b3fb543
Akshitha <akshithay16@gmail.com>
2026-03-19 15:56:18 -0500

    Initial commit: src/main/java/com/medscale/MedScaleApplication.java

---
+++ src/main/java/com/medscale/MedScaleApplication.java
@@ -0,0 +1,11 @@
+package com.medscale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedScaleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MedScaleApplication.class, args);
+    }
+}
